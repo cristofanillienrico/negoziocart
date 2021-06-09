44e76fc77752639b5e78b84a390cc0f7fddea7ff
Solving-Enrico <cristofanillienrico@gmail.com>
2021-06-09 14:55:54 +0200

    Initial commit

---
+++ src/main/java/it/prova/negoziocart/NegoziocartApplication.java
@@ -0,0 +1,13 @@
+package it.prova.negoziocart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegoziocartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegoziocartApplication.class, args);
+	}
+
+}
